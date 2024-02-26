66c8725a97d8ce5a8e2489caa73def6b54b0ce23
meandi <2358357084@qq.com>
2024-02-26 21:37:02 +0800

    lab4 backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -132,3 +132,13 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void backtrace(){
+  uint64 fp=r_fp();
+  printf("backtrace:\n");
+  while(PGROUNDUP(fp)-PGROUNDDOWN(fp)==PGSIZE){
+    uint64 ret_addr=*(uint64*)(fp-8);
+    printf("%p\n",ret_addr);
+    fp=*(uint64*)(fp-16);
+  }
+}
